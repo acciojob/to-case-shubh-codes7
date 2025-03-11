7cf24d2ad3eae683017ffb8caaf37fff106def6a
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-11 14:17:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function toCase(text) {
   // write your code here
+	if(text.length === 0) return '-';
+	text.trim()
 	return (`${text.toLowerCase()}-${text.toUpperCase()}`);
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
